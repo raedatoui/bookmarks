8fe8c190930574d5dea33e29b117e4cbd5ffbf9c
raedatoui <raed.atoui@gmail.com>
2020-01-05 17:48:49 -0500

    upgrades

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './modules';
